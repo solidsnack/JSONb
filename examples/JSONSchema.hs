4b604c6a4cd0eb7d6051c431363a8c36bd863b3b
Jason Dusek <jason.dusek@gmail.com>
2009-09-17 17:34:04 -0700

    Much more compact, meaningful output.

--- examples/JSONSchema.hs
+++ examples/JSONSchema.hs
@@ -1,29 +1,50 @@
 #!/usr/bin/env runhaskell
 
 
-import Prelude hiding (interact, unlines, tail, null)
+
+
+import Prelude hiding (interact, unlines, tail, null, unwords)
 import Data.ByteString.Lazy.Char8 hiding (reverse)
+import qualified Data.Set as Set
+
+import qualified Data.Trie as Trie
 
 import qualified JSONb
 
 
 
 
-main                         =  interact schema
+main                         =  interact (unlines . schemas . progressive)
 
 
-schema                       =  unlines . fmap name . progressive_parse
+--schema                       =  unlines . fmap name . progressive_parse
+-- where
+--  name json                  =  pack $ case json of
+--    JSONb.Object _          ->  "Object"
+--    JSONb.Array _           ->  "Array"
+--    JSONb.String _          ->  "String"
+--    JSONb.Number _          ->  "Number"
+--    JSONb.Boolean _         ->  "Boolean"
+--    JSONb.Null              ->  "Null"
+
+
+schemas                      =  Set.elems . Set.fromList . fmap schema
+
+
+schema json                  =  case json of
+  JSONb.Object trie         ->  (unwords . curly . schemas . values) trie
+  JSONb.Array list          ->  (unwords . square . schemas) list
+  JSONb.String _            ->  pack "string"
+  JSONb.Number _            ->  pack "num"
+  JSONb.Boolean _           ->  pack "bool"
+  JSONb.Null                ->  pack "null"
  where
-  name json                  =  pack $ case json of
-    JSONb.Object _          ->  "Object"
-    JSONb.Array _           ->  "Array"
-    JSONb.String _          ->  "String"
-    JSONb.Number _          ->  "Number"
-    JSONb.Boolean _         ->  "Boolean"
-    JSONb.Null              ->  "Null"
+  values                     =  fmap snd . Trie.toList
+  curly                      =  ([pack "{"] ++) . (++ [pack "}"])
+  square                     =  ([pack "["] ++) . (++ [pack "]"])
 
 
-progressive_parse            =  progressive_parse' []
+progressive                  =  progressive_parse' []
  where
   progressive_parse' acc bytes
     | null bytes             =  reverse acc
