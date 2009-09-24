a9ed92115b47c945d91cf560142e4814d670da76
Jason Dusek <jason.dusek@gmail.com>
2009-09-24 14:10:00 -0700

    Has some problems but works on some data.

--- examples/JSONSchema.hs
+++ examples/JSONSchema.hs
@@ -22,9 +22,21 @@
 
 
 
-import Prelude hiding (interact, unlines, tail, null, unwords)
-import Data.ByteString.Lazy.Char8 hiding (reverse)
+import Prelude hiding
+  ( interact
+  , lines
+  , unlines
+  , tail
+  , null
+  , unwords
+  , length
+  , repeat
+  , elem
+  , take
+  )
+import Data.ByteString.Lazy.Char8 hiding (any, reverse, foldr)
 import Data.Word
+import Data.Monoid
 import qualified Data.Set as Set
 
 import qualified Data.Trie as Trie
@@ -34,34 +46,12 @@ import qualified JSONb
 
 
 
-main                         =  interact (unlines . schemas . progressive)
-
-
---schema                       =  unlines . fmap name . progressive_parse
--- where
---  name json                  =  pack $ case json of
---    JSONb.Object _          ->  "Object"
---    JSONb.Array _           ->  "Array"
---    JSONb.String _          ->  "String"
---    JSONb.Number _          ->  "Number"
---    JSONb.Boolean _         ->  "Boolean"
---    JSONb.Null              ->  "Null"
-
-
-schemas                      =  Set.elems . Set.fromList . fmap schema
-
-
-schema json                  =  case json of
-  JSONb.Object trie         ->  (unwords . curly . schemas . values) trie
-  JSONb.Array list          ->  (unwords . square . schemas) list
-  JSONb.String _            ->  pack "str"
-  JSONb.Number _            ->  pack "num"
-  JSONb.Boolean _           ->  pack "bool"
-  JSONb.Null                ->  pack "null"
+main                         =  interact show_it
  where
-  values                     =  fmap snd . Trie.toList
-  curly                      =  ([pack "{"] ++) . (++ [pack "}"])
-  square                     =  ([pack "["] ++) . (++ [pack "]"])
+  show_it                    =  unlines . fmap display . schemas . progressive
+   where
+    schemas                 ::  [JSONb.JSON] -> [Schema OneMany]
+    schemas                  =  schemas
 
 
 progressive                  =  progressive_parse' []
@@ -73,62 +63,107 @@ progressive                  =  progressive_parse' []
       (Right piece, rem)    ->  progressive_parse' (piece:acc) rem
 
 
+schemas                     ::  (Plus t) => [JSONb.JSON] -> [Schema t]
+schemas                      =  foldr collate [] . fmap schema
 
+schema                      ::  (Plus t) => JSONb.JSON -> Schema t
+schema json                  =  case json of
+  JSONb.Object trie         ->  Obj one $ fmap schema trie
+  JSONb.Array list          ->  Arr one $ schemas list
+  JSONb.String _            ->  Str one
+  JSONb.Number _            ->  Num one
+  JSONb.Boolean _           ->  Bool one
+  JSONb.Null                ->  Null one
+ where
+  values                     =  fmap snd . Trie.toList
 
-class Collate t where
-  c                         ::  [t] -> t -> [t]
-
-class Recognize t where
-  r                         ::  JSONb.JSON -> Schema t
-
-class Display t where
-  d                         ::  [t] -> t -> [t]
 
 data Schema t
   = Num t
   | Str t
   | Bool t
   | Null t
-  | Obj t (Set.Set (Schema t))
+  | Obj t (Trie.Trie (Schema t))
   | Arr t [Schema t]
 deriving instance (Eq t) => Eq (Schema t)
 
 data OneMany                 =  One | Many
 deriving instance Eq OneMany
 
-instance Collate (Schema ()) where
-  c (Num _ : t) (Num _)      =  Num () : t
-  c (Str _ : t) (Str _)      =  Str () : t
-  c (Bool _ : t) (Bool _)    =  Bool () : t
-  c (Null _ : t) (Null _)    =  Null () : t
-  c (Obj _ a : t) (Obj _ b)  |  a == b         =  Obj () b : t
-                             |  otherwise      =  Obj () b : Obj () a : t
-  c (Arr _ a : t) (Arr _ b)  |  a == b         =  Arr () b : t
-                             |  otherwise      =  Arr () b : Arr () a : t
-  c list b                   =  b : list
 
-instance Collate (Schema OneMany) where
-  c (Num _ : t) (Num _)      =  Num Many : t
-  c (Str _ : t) (Str _)      =  Str Many : t
-  c (Bool _ : t) (Bool _)    =  Bool Many : t
-  c (Null _ : t) (Null _)    =  Null Many : t
-  c (Obj x a : t) (Obj y b)  |  a == b         =  Obj Many b : t
+collate :: (Plus t) => Schema t -> [Schema t] -> [Schema t]
+collate                      =  flip c
+ where
+  c (Num x : t) (Num y)      =  Num (x `plus` y) : t
+  c (Str x : t) (Str y)      =  Str (x `plus` y) : t
+  c (Bool x : t) (Bool y)    =  Bool (x `plus` y) : t
+  c (Null x : t) (Null y)    =  Null (x `plus` y) : t
+  c (Obj x a : t) (Obj y b)  |  a == b         =  Obj (x `plus` y) b : t
                              |  otherwise      =  Obj x b : Obj y a : t
-  c (Arr x a : t) (Arr y b)  |  a == b         =  Arr Many b : t
-                             |  otherwise      =  Arr x b : Arr y a : t
-  c list b                   =  b : list
-
-instance Collate (Schema Word) where
-  c (Num x : t) (Num y)      =  Num (x+y) : t
-  c (Str x : t) (Str y)      =  Str (x+y) : t
-  c (Bool x : t) (Bool y)    =  Bool (x+y) : t
-  c (Null x : t) (Null y)    =  Null (x+y) : t
-  c (Obj x a : t) (Obj y b)  |  a == b         =  Obj (x+y) b : t
-                             |  otherwise      =  Obj x b : Obj y a : t
-  c (Arr x a : t) (Arr y b)  |  a == b         =  Arr (x+y) b : t
+  c (Arr x a : t) (Arr y b)  |  a == b         =  Arr (x `plus` y) b : t
                              |  otherwise      =  Arr x b : Arr y a : t
   c list b                   =  b : list
 
 
+class (Eq t) => Plus t where
+  plus                      ::  t -> t -> t
+  one                       ::  t
 
+instance Plus OneMany where
+  one                        =  One
+  plus _ _                   =  Many
+
+instance Plus Word where
+  one                        =  1
+  plus                       =  (+)
+
+instance Plus () where
+  one                        =  ()
+  plus _ _                   =  ()
+
+
+class Display t where
+  display                   ::  t -> ByteString
+
+instance Display OneMany where
+  display One                =  empty
+  display Many               =  singleton '+'
+
+instance Display Word where
+  display                    =  pack . show
+
+instance Display () where
+  display _                  =  empty
+
+instance (Display t) => Display (Schema t) where
+  display (Num t)            =  unwords [pack "num", display t]
+  display (Str t)            =  unwords [pack "str", display t]
+  display (Bool t)           =  unwords [pack "bool", display t]
+  display (Null t)           =  unwords [pack "null", display t]
+  display (Obj t trie)       =  unwords [(c . b . props) trie, display t]
+  display (Arr t list)       =  unwords [s . b $ fmap display list, display t]
+
+c                            =  (pack "{ " `append`) . (`append` pack " }")
+
+s                            =  (pack "[ " `append`) . (`append` pack " ]")
+
+props :: (Display t) => Trie.Trie (Schema t) -> [ByteString]
+props                        =  Trie.toListBy prop
+ where
+  prop k v                   =  k' `append` pack ": " `append` dent v
+   where
+    dent                     =  intercalate spaces . lines . display
+    spaces                   =  take (length k' + 3) ('\n' `cons` repeat ' ')
+    k'                       =  fromChunks [k]
+
+b things                     =  if broken things || overlong things
+                                  then  intercalate (pack "\n, ") (fmap dent things)
+                                  else  intercalate (pack ", ") things
+                                 where
+                                  broken       =  any ('\n' `elem`)
+                                  overlong     =  (> len) . sum . fmap length
+                                  len          =  64
+                                  dent         =  intercalate spaces . lines
+                                   where
+                                    spaces     =  pack "\n  "
 
