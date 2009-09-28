a09c4f5f1f8ef49baa4d808c5c44747a294d6795
Jason Dusek <jason.dusek@gmail.com>
2009-09-27 23:47:47 -0700

    Made schema creation and display available outside the inner modules.

--- Text/JSONb.hs
+++ Text/JSONb.hs
@@ -7,8 +7,12 @@ module Text.JSONb
   ( Text.JSONb.Simple.JSON(..)
   , Text.JSONb.Decode.decode
   , Text.JSONb.Decode.break
-  , Text.JSONb.Encode.encode
   , Text.JSONb.Encode.Style(..)
+  , Text.JSONb.Encode.encode
+  , Text.JSONb.Schema.Schema()
+  , Text.JSONb.Schema.schema
+  , Text.JSONb.Schema.schemas
+  , Text.JSONb.Schema.Display.Display(..)
   , Text.JSON.Escape.Escape(..)
   , Text.JSON.Escape.escaped
   ) where
@@ -17,7 +21,8 @@ module Text.JSONb
 import Text.JSONb.Simple
 import Text.JSONb.Decode
 import Text.JSONb.Encode
---import Text.JSONb.Schema
+import Text.JSONb.Schema
+import Text.JSONb.Schema.Display
 import Text.JSON.Escape
 
 
